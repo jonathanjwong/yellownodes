76fa50d0921fdd73747863097bb1440cb992ac6f
yellownanners <jjwong@ucdavis.edu>
2015-04-20 18:09:37 -0700

    Comments/Notes
    Ready for actual testing and programming at home

--- Node Practice/program.js
+++ Node Practice/program.js
@@ -7,8 +7,11 @@ The list of files should be printed to the console, one file per line. You must 
 */
 
 var fs = require('fs');
+var path = require('path');
 var path = process.argv[2];
 fs.readdir(path, function (err, list) {
   console.log(list);      //gives me the full list
   console.log(process.argv[3]);     //checking what file type that needs filtering
+  path.extname(list); //go through the array [list] and check if the extnames match...use [list.length] for array length...use first i/o program.js as a reference for that. 
+  // '.' + process.argv[3] works in adding the period and changing into a string
 });
