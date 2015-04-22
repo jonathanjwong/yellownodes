142a9b6ad28215254fe0731f8bc3a2d086f53de2
yellownanners <jjwong@ucdavis.edu>
2015-04-22 12:49:09 -0700

    comments

--- Nodular/programASYNCIO.js
+++ Nodular/programASYNCIO.js
@@ -3,7 +3,7 @@ var path = process.argv[2];
 fs.readFile(path, 'utf8', function (err, data) {
   //var lines = data.split('\n');
   //console.log(lines.length - 1);
-  console.log(err);
+  console.log(err); //trying to understand err functionality
 });
 
 
