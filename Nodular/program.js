f2c22dba6a2c3a41be4d10306d8326d361767272
yellownanners <jjwong@ucdavis.edu>
2015-04-23 10:18:18 -0700

    http client first test

--- Nodular/program.js
+++ Nodular/program.js
@@ -1,3 +1,13 @@
+var http = require('http');
+var get = process.argv[2];
+
+http.get (get, function (getterino) {
+  getterino.setEncoding('utf8');
+  getterino.on ('data', function (data) {
+    console.log (data);
+  });
+});
+/*
 Write a program that performs an HTTP GET request to a URL provided to you as the first command-line argument. Write the String contents of each "data" event from the response to a new line on the console (stdout).
 
 -------------------------------------------------------------------------------
@@ -13,12 +23,13 @@ The http.get() method is a shortcut for simple GET requests, use it to simplify 
 
 Unlike other callback functions, this one has the signature:
 
-    function callback (response) { /* ... */ }
+    function callback (response) { ... }
 
 Where the response object is a Node Stream object. You can treat Node Streams as objects that emit events. The three events that are of most interest are: "data", "error" and "end". You listen to an event like so:
 
-    response.on("data", function (data) { /* ... */ })
+    response.on("data", function (data) { ... })
 
 The "data" event is emitted when a chunk of data is available and can be processed. The size of the chunk depends upon the underlying data source.
+*/
 
 The response object / Stream that you get from http.get() also has a setEncoding() method. If you call this method with "utf8", the "data" events will emit Strings rather than the standard Node Buffer objects which you have to explicitly convert to Strings.
